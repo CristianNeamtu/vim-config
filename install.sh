6c43504a6716b058d99f62309828c4c0a43574fe
Cristian Neamtu <cristian.neamtu.93@gmail.com>
2015-12-14 21:54:52 +0200

    Updated installation script (ycm compiling)

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# Set up symlinks
 ln -sfn ~/.config/vim-config/ ~/.vim
 ln -sfn ~/.config/vim-config/_vimrc  ~/.vimrc
 
+# Clone vundle
 git clone https://github.com/gmarik/vundle.git ~/.config/vim-config/bundle/vundle
 
+# Install plugins
 vim +BundleInstall +qall
 
-echo "execute !python2 ~/.config/vim-config/bundle/YouCompleteMe/install.py --clang-completer"
+# Compiling YCM
+python2 ~/.config/vim-config/bundle/YouCompleteMe/install.py --clang-completer
